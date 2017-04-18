b19cb44e3b957ec64a18645c65656d888fa79eff
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-04-18 18:00:56 +0200

    Init git 2

---
+++ ProjectTimeAssistant/Services/DataBase/DataContext.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using System.Data.Entity;
+//using ProjectTimeAssistant.Models;
+
+//namespace ProjectTimeAssistant.Services.DataBase
+//{
+//    class DataContext : DbContext
+//    {
+//        public DbSet<Issue> Issues { get; set; }
+//        public DbSet<Project> Projects { get; set; }
+//        public DbSet<WorkTime> WorkTimes { get; set; }
+//    }
+
+//}
