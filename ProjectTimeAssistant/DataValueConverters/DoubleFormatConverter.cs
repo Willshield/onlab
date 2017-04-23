efa20ae6b16736579783e06fc211d67a1e1d4d3c
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-04-24 01:24:07 +0200

    double formatter implemented

---
+++ ProjectTimeAssistant/DataValueConverters/DoubleFormatConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ProjectTimeAssistant.DataValueConverters
+{
+    class DoubleFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+            
+            return string.Format("{0:0.00}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
