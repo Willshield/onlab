3dcd6dfcdbcb94f198991e1fc6757a3f02274597
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-04-26 01:58:51 +0200

    ActuallyTrackingPage GUI plan

---
+++ ProjectTimeAssistant/ViewModels/ActuallyTrackingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace ProjectTimeAssistant.ViewModels
+{
+    class ActuallyTrackingViewModel : ViewModelBase
+    {
+
+
+    }
+}
